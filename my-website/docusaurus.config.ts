f8545598385cc2b568f14dde953c0908cffb3548
sun-guannan <sguann@qq.com>
2024-08-20 16:42:15 +0800

    install linux

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -3,7 +3,7 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: '店铺',
+  title: 'AI小创客',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
@@ -58,7 +58,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: '店铺',
+      title: 'AI小创客',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
